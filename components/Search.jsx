a8b55dce397336e7230491f08ecb808dbe6cd637
Harshit Mittal <152404639+Harshit-Mitta@users.noreply.github.com>
2025-01-30 23:31:16 +0530

    Add files via upload

---
+++ components/Search.jsx
@@ -0,0 +1,22 @@
+import {useEffect, useState} from 'react'
+import styles from './search.module.css'
+const URL='https://api.spoonacular.com/recipes/complexSearch';
+const API_KEY='516c7c864e664acc954974e048ac46aa';
+export default function Search({foodData,setFood}){
+    const [query,setQuery] = useState("pizza");
+    useEffect(()=>{
+        async function fetchFood(){
+            const res= await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+            const data= await res.json();
+            console.log(data.results);
+            setFood(data.results);
+        }
+        fetchFood();
+    },[query]);
+    return (<div className={styles.search}> 
+        <input className={styles.input}
+        value={query} 
+        onChange={(e)=>setQuery(e.target.value)}
+        type="text" />
+    </div>)
+}
